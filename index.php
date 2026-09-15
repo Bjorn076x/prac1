cbd4462fd16a3bc4cd5346c616d886c3b4b0c11a
Bjorn076x <Bvklaveren@icloud.com>
2026-09-15 11:12:50 +0200

    body/header

--- index.php
+++ index.php
@@ -10,14 +10,23 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header>
+    <header class="hero">
+        <nav class="navbar">
+            <div class="logo">Curio Software Developer</div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Opleidingen</a></li>
+                <li><a href="#">Open Dag</a></li>
+                <li><a href="#">Studentenleven</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <a class="cta" href="#">Kom langs!</a>
+        </nav>
 
+        <div class="hero-content">
+            <h1>Code your future.</h1>
+            <p>Ontdek jouw toekomst in de IT tijdens de Curio Open Dag.</p>
+        </div>
     </header>
-    <body>
-        
-    </body>
-    <footer>
-
-    </footer>
 </body>
 </html>
